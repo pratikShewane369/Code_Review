482817409a1545c4d5715a703bd60f127a06dd10
pratikShewane369 <mohanshewane@gmail.com>
2026-08-29 13:25:54 +0530

    Added Code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const app = require('./src/app.js');
+const port = process.env.PORT || 30000;
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(port, () => {
+    console.log(`Server is running on port 3000 ${port}`);
 })
